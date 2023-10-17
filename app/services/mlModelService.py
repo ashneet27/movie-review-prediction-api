2f1e92ebfad1e0d04a356c0b9ee9e89457db501d
Ashneet Singh <ashneet.singh@oracle.com>
2023-10-17 16:01:58 +0530

    switch to sklearn's multinomial nb model

--- app/services/mlModelService.py
+++ app/services/mlModelService.py
@@ -1,38 +1,45 @@
 import os
-import numpy as np
+
 import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
 
-from app.ml.multinomialNaiveBayes import MultinomialNaiveBayes
 from app.ml.tokenizer import Tokenizer
 from app.models.mlModel import MlModel
 
 
 class MlModelService:
     RELATIVE_PATH_TO_TRAINING_DATA = "../data/labeledTrainData.tsv"
-    TRAINING_DATA = pd.read_csv(os.path.join(os.path.dirname(__file__), RELATIVE_PATH_TO_TRAINING_DATA), delimiter="\t")
+    TRAINING_DATA = pd.read_csv(
+        os.path.join(os.path.dirname(__file__), RELATIVE_PATH_TO_TRAINING_DATA),
+        delimiter="\t",
+    )
 
-    X = TRAINING_DATA["review"].values
-    Y = TRAINING_DATA["sentiment"].values
+    X = TRAINING_DATA["review"].apply(lambda x: " ".join(Tokenizer().tokenize(x)))
+    Y = TRAINING_DATA["sentiment"]
+
     RANDOM_SEED = 42
+    X_TRAIN, X_TEST, Y_TRAIN, Y_TEST = train_test_split(
+        X, Y, test_size=0.2, random_state=RANDOM_SEED
+    )
 
-    X_TRAIN, X_TEST, Y_TRAIN, Y_TEST = train_test_split(X, Y, test_size=0.2, random_state=RANDOM_SEED)
+    VECTORIZER = CountVectorizer(max_features=100000)
+    X_TRAIN_VECTORS = VECTORIZER.fit_transform(X_TRAIN)
 
+    MNB = MultinomialNB().fit(X_TRAIN_VECTORS, Y_TRAIN)
 
-    MNB = MultinomialNaiveBayes(
-        classes=np.unique(Y),
-        tokenizer=Tokenizer()
-    ).fit(X_TRAIN, Y_TRAIN)
-
-    Y_HAT: list = None
+    Y_HAT = MNB.predict(VECTORIZER.transform(X_TEST))
 
     @classmethod
     def modelInfo(cls) -> MlModel:
         return MlModel(accuracy=cls.calculateAccuracy())
-    
+
     @classmethod
     def calculateAccuracy(cls) -> float:
-        if cls.Y_HAT == None:
-            cls.Y_HAT = cls.MNB.predict(cls.X_TEST)
         return accuracy_score(cls.Y_HAT, cls.Y_TEST)
+
+    @classmethod
+    def predict(cls, X: list) -> list:
+        return list(cls.MNB.predict(cls.VECTORIZER.transform(X)))
